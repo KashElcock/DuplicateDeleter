157e16dfcc724e896b8ebdb72b72144588d12eb2
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-29 20:54:34 -0500

    attempted, must review and return

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,28 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+        Integer[] temp = new Integer[intArray.length];
+        int j = 0;
+
+        for (int i = 0; i < intArray.length-1; i++) {
+            if(intArray[i] != intArray[i+1]){
+                temp[j++] = intArray[i];
+            }
+        } temp[j++] = intArray[intArray.length-1];
+        for (int k = 0; k < j; k++) {
+            System.out.println(temp[k]);
+        }
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return new Integer[0];
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new Integer[0];
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,26 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+        Integer[] temp = new Integer[intArray.length];
+        int j = 0;
+
+        for (int i = 0; i < intArray.length-1; i++) {
+            if(intArray[i] != intArray[i+1]){
+                temp[j++] = Integer.valueOf(intArray[i]);
+            }
+        } temp[j++] = Integer.valueOf(intArray[intArray.length-1]);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+
+        return new String[0];
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return new String[0];
+    }
 }
